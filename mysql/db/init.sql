e92c107d0939e97e40c22a0108dd9373dec988b8
Yangwook Jeong <yangwookee@gmail.com>
2021-05-31 14:09:58 +0900

    -

---
+++ mysql/db/init.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8;
+
+CREATE DATABASE IF NOT EXISTS `test`;
+SET character_set_client = utf8mb4;
+
+USE `test`;
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+SELECT plugin FROM mysql.user WHERE User = 'root';
+FLUSH PRIVILEGES;
