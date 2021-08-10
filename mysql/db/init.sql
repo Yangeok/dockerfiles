88cb8ff5752eed6baf54ba83bf30e4fd4dc272c5
Yangwook Jeong <yangwookee@gmail.com>
2021-08-10 21:20:14 +0900

    Add hadoop images

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -9,3 +9,5 @@ ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 SELECT plugin FROM mysql.user WHERE User = 'root';
 FLUSH PRIVILEGES;
+
+INSTALL SONAME 'ha_mroonga';
